138ed7ea4b8d63737a8020f2c6a15d6ceb361d58
Ivan Perez <ivan.perez@keera.co.uk>
2024-09-12 00:52:45 +0000

    Update source file, sha, homepage, order of instructions, path to copilot man page

--- copilot.rb
+++ copilot.rb
@@ -1,19 +1,19 @@
 class Copilot < Formula
-  homepage "https://github.com/nasa-sw-vnv/ikos"
-  url "https://github.com/NASA-SW-VnV/ikos/archive/refs/tags/v3.3.tar.gz"
-  sha256 "27b26dede55cbbda2c090d268db430dd64c01c663ea185c1a07e4c95eda15cd5"
-  license "NOSA"
+  homepage "https://github.com/copilot-language/copilot"
+  url "https://github.com/ivanperez-keera/copilot/archive/refs/heads/develop-homebrew.tar.gz"
+  sha256 "2addad1ce0bfea20598b9d84866bee47e12118bcaa11f3a7a041e397fae00063"
+  license "BSD"
   revision 1
 
   depends_on "cabal-install" => :build
   depends_on "ghc@9.8" => :build
 
   def install
+    man1.install "doc/copilot.1"
     system "cabal", "update"
     system "cabal", "install", "--verbose", "--lib", *std_cabal_v2_args, "copilot"
     system "ls"
     system "pwd"
-    man1.install "../copilot.1"
   end
 
 end
