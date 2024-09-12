b4f3b14447e2c8505a9e3f533e8c8042b01b0c17
Ivan Perez <ivan.perez@keera.co.uk>
2024-09-12 05:41:46 +0000

    Use quotes to wrap version number

--- copilot.rb
+++ copilot.rb
@@ -1,10 +1,10 @@
 class Copilot < Formula
   homepage "https://github.com/copilot-language/copilot"
-  url "https://github.com/ivanperez-keera/copilot/archive/refs/heads/develop-homebrew.tar.gz"
-  sha256 "2addad1ce0bfea20598b9d84866bee47e12118bcaa11f3a7a041e397fae00063"
   license "0BSD"
   revision 1
-  version 4.0
+  version "4.0"
+  url "https://github.com/ivanperez-keera/copilot/archive/refs/heads/develop-homebrew.tar.gz"
+  sha256 "2addad1ce0bfea20598b9d84866bee47e12118bcaa11f3a7a041e397fae00063"
 
   depends_on "cabal-install" => :build
   depends_on "ghc@9.8" => :build
